d652f1cb3e640dca4278c2a3b8c769514671beec
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-07-08 20:06:04 -0300

    Implementing h2-console

--- src/main/java/com/marcos/biblioteca/project/model/Editora.java
+++ src/main/java/com/marcos/biblioteca/project/model/Editora.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,6 +14,7 @@ import jakarta.persistence.Table;
 public class Editora implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
