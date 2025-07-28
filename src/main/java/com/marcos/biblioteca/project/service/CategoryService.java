0032b1dc536fae39af980739682c9a6a6ebd6e02
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-07-28 18:43:14 -0300

    Implementing resource classes

--- src/main/java/com/marcos/biblioteca/project/service/CategoryService.java
+++ src/main/java/com/marcos/biblioteca/project/service/CategoryService.java
@@ -1,9 +1,28 @@
 package com.marcos.biblioteca.project.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.marcos.biblioteca.project.model.Category;
+import com.marcos.biblioteca.project.repositories.CategoryRepository;
+
 
 @Service
 public class CategoryService {
 
-
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
 }
--- src/main/java/com/marcos/biblioteca/project/service/PublisherService.java
+++ src/main/java/com/marcos/biblioteca/project/service/PublisherService.java
@@ -1,8 +1,24 @@
 package com.marcos.biblioteca.project.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.marcos.biblioteca.project.model.Publisher;
+import com.marcos.biblioteca.project.repositories.PublisherRepository;
 
 @Service
 public class PublisherService {
 
+	private PublisherRepository repository;
+	
+	public List<Publisher> findAll() {
+		return repository.findAll();
+	}
+	
+	public Publisher findById(Long id) {
+		Optional<Publisher> obj = repository.findById(id);
+		return obj.get();
+	}
 }
