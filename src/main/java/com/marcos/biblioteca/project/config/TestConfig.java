dd31f4122d93e9c227b2e97be49c38407db20100
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-08-18 18:42:02 -0300

    Instantiation of the book class

--- src/main/java/com/marcos/biblioteca/project/config/TestConfig.java
+++ src/main/java/com/marcos/biblioteca/project/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.marcos.biblioteca.project.config;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.marcos.biblioteca.project.model.Author;
+import com.marcos.biblioteca.project.model.Book;
 import com.marcos.biblioteca.project.model.Publisher;
 import com.marcos.biblioteca.project.repositories.AuthorRepository;
+import com.marcos.biblioteca.project.repositories.BookRepository;
 import com.marcos.biblioteca.project.repositories.PublisherRepository;
 
 @Configuration
@@ -22,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private PublisherRepository publisherRepository; 
 	
+	@Autowired
+	private BookRepository bookRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -32,6 +38,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		authorRepository.saveAll(Arrays.asList(author1, author2, author3, author4));
 		
+		Book book1 = new Book(null, "A Rosa do povo",LocalDate.of(1945, 10, 25));
+		Book book2 = new Book(null, "Romeu e Julieta",LocalDate.of(1997, 5, 10));
+		Book book3 = new Book(null, "O Homem que ri",LocalDate.of(1869, 4, 25));
+		Book book4 = new Book(null, "Laços de família",LocalDate.of(1960, 6, 7));
+		
+		bookRepository.saveAll(Arrays.asList(book1, book2, book3, book4));
 		
 		Publisher publisher1 = new Publisher(null, "Editorial Record");
 		Publisher publisher2 = new Publisher(null, "Penguin-Companhia");
@@ -39,6 +51,8 @@ public class TestConfig implements CommandLineRunner {
 		Publisher publisher4 = new Publisher(null, "Rocco Digital");
 		
 		publisherRepository.saveAll(Arrays.asList(publisher1, publisher2, publisher3, publisher4));
+		
+		
 	}
 
 }
