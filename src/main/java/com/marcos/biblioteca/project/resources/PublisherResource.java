084b7e778891bd0300af1cd537fe3b3fbfdb24b0
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-08-18 19:38:21 -0300

    Implementing the findById() method

--- src/main/java/com/marcos/biblioteca/project/resources/PublisherResource.java
+++ src/main/java/com/marcos/biblioteca/project/resources/PublisherResource.java
@@ -3,6 +3,7 @@ package com.marcos.biblioteca.project.resources;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,16 @@ public class PublisherResource {
 
 	public PublisherService service;
 	
+	@GetMapping()
 	public ResponseEntity<List<Publisher>> findAll() {
 		List<Publisher> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = {"/id"})
+	public ResponseEntity<Publisher> findById(Long id) {
+		Publisher obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
