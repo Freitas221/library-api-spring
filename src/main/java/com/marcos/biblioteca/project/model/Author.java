e47ab54150f3634b48e1a5935bf3c3ac7281667f
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-08-18 19:14:31 -0300

    Implementing @JsonIgnore

--- src/main/java/com/marcos/biblioteca/project/model/Author.java
+++ src/main/java/com/marcos/biblioteca/project/model/Author.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +25,7 @@ public class Author implements Serializable{
 	private String nome;
 	private String nacionalidade;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "author")
 	List<Book> book = new ArrayList<>();
 	
