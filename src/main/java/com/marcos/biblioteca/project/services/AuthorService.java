f9ee2938a5647ba0c6dcf15c71b82d6fc3d8175f
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-11-24 18:49:04 -0300

    Updating findById method

--- src/main/java/com/marcos/biblioteca/project/services/AuthorService.java
+++ src/main/java/com/marcos/biblioteca/project/services/AuthorService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.marcos.biblioteca.project.model.Author;
 import com.marcos.biblioteca.project.repositories.AuthorRepository;
+import com.marcos.biblioteca.project.services.exception.ResourceNotFoundException;
 
 @Service
 public class AuthorService {
@@ -21,7 +22,7 @@ public class AuthorService {
 	
 	public Author findById(Long id) {
 		Optional<Author> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public Author insert(Author obj) {
