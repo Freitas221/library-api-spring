cd12766015f3a1437ddaa5bac2f5f557b2371865
marcosfreitas221 <marcosfreitas7542@gmail.com>
2026-06-01 19:53:32 -0300

    feat(LoanService): Implementing method existByUserAndStatus part 2

--- src/main/java/com/marcos/biblioteca/project/repositories/LoanRepository.java
+++ src/main/java/com/marcos/biblioteca/project/repositories/LoanRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.marcos.biblioteca.project.enums.LoanStatus;
 import com.marcos.biblioteca.project.model.Book;
 import com.marcos.biblioteca.project.model.Loan;
+import com.marcos.biblioteca.project.model.User;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
 	boolean existsByBookAndStatus(Book book, LoanStatus loanStatus);
+	
+	boolean existsByUserAndStatus(User user, LoanStatus loanStatus);
 }
